cf1ac4e2d4ef03a6009df2a81d7dffd8e13588f9
M.V John <john@klairvoyant.com>
2011-07-14 19:18:50 +0530

    modi

--- db/migrate/20110714134445_change_col.rb
+++ db/migrate/20110714134445_change_col.rb
@@ -1,7 +1,6 @@
 class ChangeCol < ActiveRecord::Migration
   def self.up
-    remove_column :developers, :userID
-    add_column :developers, :userid, :integer
+     add_column :developers, :userid, :integer
   end
 
   def self.down
