2aaef7a07a9d42f91ebe347a59abb54cea96dffe
M.V John <john@klairvoyant.com>
2011-07-18 18:10:45 +0530

    july-18

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110716100752) do
+ActiveRecord::Schema.define(:version => 20110718123650) do
 
   create_table "admin_people", :force => true do |t|
     t.integer  "userID"
